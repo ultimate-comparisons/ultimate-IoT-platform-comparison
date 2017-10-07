5f980e697b9dc5a22d0279cbebf7fa7ff59de233
Ultimate-Comparison-Genie <kopp.dev+ultimate-comparison-genie@gmail.com>
2017-10-07 18:04:36 +0000

    Travis commit for travis-update

--- app/components/output/html-citation-text/html-citation-text.component.ts
+++ app/components/output/html-citation-text/html-citation-text.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, ChangeDetectionStrategy } from "@angular/core";
-import { ComparisonCitationService } from "./../../comparison/components/comparison-citation.service";
+import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
+import { ComparisonCitationService } from './../../comparison/components/comparison-citation.service';
+import { ComparisonService } from './../../comparison/components/comparison.service';
 
 @Component({
     selector: 'htmlcitationtext',
@@ -8,6 +9,19 @@ import { ComparisonCitationService } from "./../../comparison/components/compari
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HtmlCitationTextComponent {
-    @Input() description: string = " ";
+    @Input() description = ' ';
     @Input() citationServ: ComparisonCitationService;
+
+    constructor(public serv: ComparisonService) {
+    }
+
+    private makeMarkdown(text: string): string {
+        if (text === null || text === undefined || text === '' || text === ' ') {
+            return ' ';
+        }
+            if (typeof text === 'object') {
+                text = text['tag'];
+        }
+        return this.serv.converter.makeHtml(text);
+    }
 }
